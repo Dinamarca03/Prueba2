6c5ba528d2e4231b667703b320f5083e413d0104
Javier <javierdinamarcaxd@gmail.com>
2025-07-08 15:00:53 -0400

    Desactiva guard global temporalmente para pruebas

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ApiKeyGuard } from './common/guards/api-key.guard'; // Ajusta la ruta si es necesario
+//import { ApiKeyGuard } from './common/guards/api-key.guard'; // Ajusta la ruta si es necesario
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.useGlobalGuards(new ApiKeyGuard(configService));
+ // app.useGlobalGuards(new ApiKeyGuard(configService));
   app.useGlobalPipes(new ValidationPipe({ 
     transform: true,
     whitelist: true,

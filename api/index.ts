f00778d115ffba6df3c7bc859e2f72102b99944f
Javier <javierdinamarcaxd@gmail.com>
2025-07-09 16:11:46 -0400

    se aplica el handler

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { ValidationPipe } from '@nestjs/common';
+
+const server = express();
+let isNestReady = false;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  await app.init();
+  isNestReady = true;
+}
+
+export default async function handler(req, res) {
+  if (!isNestReady) {
+    await bootstrap();
+  }
+  server(req, res);
+} 
